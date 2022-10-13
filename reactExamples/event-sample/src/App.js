bde0f51796029d541c3639e7046119957133584d
pnichols789 <pnichols789@gmail.com>
2022-10-12 19:13:38 -0500

    -

--- reactExamples/event-sample/src/App.js
+++ reactExamples/event-sample/src/App.js
@@ -5,26 +5,30 @@ import './App.css';
 
 function App() {
   const [counter, myFunction] = useState(0);
-  const handlePlus=()=>{
-    myFunction(counter+1)
-  };
-  const handleMinus=()=>{
-    if (counter>0){
-      myFunction(counter-1)
-    } else{
-      alert('Counter is already at minimum value of 0')
+  const handlePlus = () =>{
+    if (counter < 20){
+      myFunction(counter+1)
+    }else{
+      alert('Counter is at the maximum value')
     }
   };
-  const handleReset=()=>{
-    myFunction(0);
-  };
+  const handleMinus = () => {
+    if (counter > 0){
+      myFunction(counter-1)
+    }else{
+      alert('Counter is at the minimum value')
+    }
+  }
+  const handleReset = () =>{
+    myFunction(0)
+  }
   return (
     <div className="App">
       <h3>{counter}</h3>
       <div className="buttonGroup">
         <button onClick={handlePlus}>+</button>
         <button onClick={handleMinus}>-</button>
-        <button onClick={handleReset}>reset</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
     </div>
   );
